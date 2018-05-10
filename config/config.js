e716e776c6d07b7ae543c753187fff679b96bee1
Bogdan Bembenok <Bogdan_Bembenok@epam.com>
2018-05-10 11:05:55 +0300

    merge

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 const configs = {
   local: {
     baseHref: '/',
-    apiUrl: 'http://159.89.5.152:4200/api/',
+    apiUrl: 'http://localhost:4200/api/',
     googleAnalyticsTag: '-',
     sourceMap: 'nosources-source-map'
   },
   dev: {
     baseHref: '/',
-    apiUrl: 'http://159.89.5.152:4200/api/',
+    apiUrl: 'http://localhost:4200/api/',
     googleAnalyticsTag: '-',
     sourceMap: 'nosources-source-map'
   },
   prod: {
     baseHref: '/',
-    apiUrl: 'http://159.89.5.152:4200/api/',
+    apiUrl: 'http://localhost:4200/api/',
     googleAnalyticsTag: '-',
     sourceMap: 'nosources-source-map'
   },
