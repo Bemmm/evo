f0786dc797d91c5897d43a722cb7f266d7cfcf10
Bogdan Bembenok <Bogdan_Bembenok@epam.com>
2018-05-14 16:41:45 +0300

    fixed endponits

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 const configs = {
   local: {
     baseHref: '/',
-    apiUrl: 'http://carswork.com:4200/api/',
+    apiUrl: 'https://carswork.com/api/',
     googleAnalyticsTag: '-',
     sourceMap: 'nosources-source-map'
   },
   dev: {
     baseHref: '/',
-    apiUrl: 'http://carswork.com:4200/api/',
+    apiUrl: 'https://carswork.com/api/',
     googleAnalyticsTag: '-',
     sourceMap: 'nosources-source-map'
   },
   prod: {
     baseHref: '/',
-    apiUrl: 'http://carswork.com:4200/api/',
+    apiUrl: 'https://carswork.com/api/',
     googleAnalyticsTag: '-',
     sourceMap: 'nosources-source-map'
   },
